901688413de5e6fb3e37aaf4d5bc8b4391cdde57
EvangelosGeorgiou <tagishav@hotmail.com>
2020-11-10 12:58:36 +0200

    testing!!!

--- app/src/main/java/com/example/cyprusreservations/MainActivity.java
+++ app/src/main/java/com/example/cyprusreservations/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
     private CustomAdaptor customAdaptor;
 
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
