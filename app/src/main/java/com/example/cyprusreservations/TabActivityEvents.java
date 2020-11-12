493be9b040971abeafc9c9eed7e8486badbd7ba8
RafaellosDem <74240586+RafaellosDem@users.noreply.github.com>
2020-11-12 22:49:22 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/fragment_events.xml

--- app/src/main/java/com/example/cyprusreservations/TabActivityEvents.java
+++ app/src/main/java/com/example/cyprusreservations/TabActivityEvents.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Events#newInstance} factory method to
+ * Use the {@link TabActivityEvents#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Events extends Fragment {
+public class TabActivityEvents extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class Events extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Events() {
+    public TabActivityEvents() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class Events extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Events.
+     * @return A new instance of fragment TabActivityEvents.
      */
     // TODO: Rename and change types and number of parameters
-    public static Events newInstance(String param1, String param2) {
-        Events fragment = new Events();
+    public static TabActivityEvents newInstance(String param1, String param2) {
+        TabActivityEvents fragment = new TabActivityEvents();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class Events extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events, container, false);
+        return inflater.inflate(R.layout.fragment_tab_activity_events, container, false);
     }
 }
